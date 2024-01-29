cb6b564dcdd5c300688d04d74b646bc64d259495
Ben Eberle <eberlebe@umich.com>
2024-01-29 16:46:17 -0700

    latest updates

--- superset-init.sh
+++ superset-init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Initialize the database
+superset db upgrade
+
 # Create an admin user (you will be prompted to set a username, first and last name before setting a password)
 superset fab create-admin \
               --username admin \
@@ -8,11 +11,8 @@ superset fab create-admin \
               --email eberlebe@umich.edu \
               --password test
 
-# Initialize the database
-superset db upgrade
-
 # Create default roles and permissions
 superset init
 
 # To start a development web server, use the -p option to bind to another port
-superset run -p 8088 --with-threads --reload --debugger
+superset run -p 8080 --with-threads --reload --debugger
