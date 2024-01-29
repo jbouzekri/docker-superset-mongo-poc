b243bf1cea6771e720a28c99e3e79579cb07c137
Ben Eberle <eberlebe@umich.com>
2024-01-29 14:52:12 -0700

    fix

--- superset_config.py
+++ superset_config.py
@@ -14,7 +14,7 @@ SECRET_KEY = secrets.token_urlsafe(16)
 # The SQLAlchemy connection string to your database backend
 # This connection defines the path to the database that stores your
 # superset metadata (slices, connections, ...)
-SQLALCHEMY_DATABASE_URI = 'awsathena+rest://:@athena.us-east-1.amazonaws.com/data?s3_staging_dir=s3://reckoning-s3-bucket/query_results'
+SQLALCHEMY_DATABASE_URI = 'awsathena+rest://:@athena.us-east-1.amazonaws.com/reckoning-glue-db?s3_staging_dir=s3://reckoning-s3-bucket/query_results'
 
 # Flask-WTF flag for CSRF
 WTF_CSRF_ENABLED = True
