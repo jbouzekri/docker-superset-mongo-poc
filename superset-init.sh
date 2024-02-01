81eca4ab7a5f62116669d3434a0493ab697c1979
Ben Eberle <eberlebe@umich.com>
2024-01-31 19:49:00 -0700

    feat: set flash app

--- superset-init.sh
+++ superset-init.sh
@@ -3,6 +3,9 @@
 # Initialize the database
 superset db upgrade
 
+# set flask app
+export FLASK_APP=superset
+
 # Create an admin user (you will be prompted to set a username, first and last name before setting a password)
 superset fab create-admin \
               --username admin \
